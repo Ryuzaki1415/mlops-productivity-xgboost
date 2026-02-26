9ccc0f833a445d730c61027996fca65a7fbc632d
dheeraj <dheerajnunni@gmail.com>
2026-02-26 20:53:12 +0530

    feat: Added Evals

---
+++ evaluate.py
@@ -0,0 +1,13 @@
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
+
+
+def evaluate_model(model, X_test, y_test):
+
+    predictions = model.predict(X_test)
+
+    rmse = np.sqrt(mean_squared_error(y_test, predictions))
+    mae = mean_absolute_error(y_test, predictions)
+    r2 = r2_score(y_test, predictions)
+
+    return rmse, mae, r2
